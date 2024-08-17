488c0508573e0623206ab78e08b32a6c93af3e24
Volodya Mikhnov <vvvmikhnov@gmail.com>
2024-08-17 23:39:03 +0300

    .

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import { setupMiddlewares } from './src/middlewares/middleware.js'
+import connectDB from './src/database/database.js'
+import morgan from 'morgan'
+import cors from 'cors'
+
+async function createServer() {
+   const app = express()
+
+   app.use(
+      cors({
+         exposedHeaders: ['Authorization'],
+      }),
+   )
+   app.use(morgan('dev'))
+   app.use(express.json())
+
+   await connectDB()
+   await setupMiddlewares(app)
+
+   app.listen(3000, () => {
+      console.log('Server is running on http://localhost:3000')
+   })
+}
+
+createServer()

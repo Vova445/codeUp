e447469573356da5ec5073c0547811884922d263
Volodya Mikhnov <vvvmikhnov@gmail.com>
2024-08-18 11:37:23 +0300

    .

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,12 @@ import { setupMiddlewares } from './src/middlewares/middleware.js';
 import connectDB from './src/database/database.js';
 import morgan from 'morgan';
 import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
+
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 
 async function createServer() {
     const app = express();
@@ -20,14 +18,6 @@ async function createServer() {
     }));
     app.use(morgan('dev'));
     app.use(express.json());
-
-    if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, '../frontend/dist')));
-        
-        app.get('*', (req, res) => {
-            res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-        });
-    }
 
     await connectDB();
     await setupMiddlewares(app);
--- backend/src/middlewares/middleware.js
+++ backend/src/middlewares/middleware.js
@@ -1,30 +1,28 @@
-import { createServer as createViteServer } from 'vite'
+import express from 'express'
 import { errorHandler } from '../error/errorHandler.js'
 import userRoutes from '../routes/userRoutes.js'
-// import { setupMiddlewares } from './setupMiddleware.js'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+''
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export async function setupMiddlewares(app) {
-   const vite = await createViteServer({
-      server: { middlewareMode: true },
-   })
+  
 
    app.use('/api', userRoutes)
 
-   app.use(vite.middlewares)
+   if (process.env.NODE_ENV === 'production') {
+      app.use(express.static(path.join(__dirname, '../../../frontend/dist')));
+      
+      app.get('*', (req, res) => {
+          res.sendFile(path.join(__dirname, '../../../frontend/dist/index.html'));
+      });
+  }
 
-   app.use('*', async (req, res) => {
-      const url = req.originalUrl
-      try {
-         let template = await vite.transformIndexHtml(
-            url,
-            `<!DOCTYPE html><html><head><title>Vue App</title></head><body><div id="app"></div></body></html>`,
-         )
-         res.status(200).set({ 'Content-Type': 'text/html' }).end(template)
-      } catch (e) {
-         vite.ssrFixStacktrace(e)
-         console.error(e)
-         res.status(500).end(e.message)
-      }
-   })
    app.use(errorHandler)
 }

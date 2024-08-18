2652e4a3c30f5d972168dad27e0eb444543d3f8a
Andri Kocherha <108304490+AndrijKocherha@users.noreply.github.com>
2024-08-18 17:43:40 +0300

    d

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,38 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
    plugins: [vue()],
-   server: {
-      historyApiFallback: true,
+   resolve: {
+      alias: {
+         '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+      // 'process.env': {
+      //    VITE_BASE_URL: process.env.VITE_BASE_URL || 'https://main--mybestcodeup.netlify.app',
+      // },
    },
-   // Якщо ви використовуєте проксі для API запитів
-   // proxy: {
-   //   '/api': {
-   //     target: 'http://localhost:5000',
-   //     changeOrigin: true,
-   //     rewrite: (path) => path.replace(/^\/api/, '')
-   //   }
-   // }
+   build: {
+      outDir: 'dist',
+   },
+
+   define: {
+      'process.env': {
+         VITE_API_URL: process.env.VITE_API_URL,
+      },
+   },
 })
+//export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//      alias: {
+//         '@': fileURLToPath(new URL('./src', import.meta.url)),
+//      },
+//   },
+//   build: {
+//      outDir: 'dist',
+//   },
+//   base: '/codeup/',
+//})

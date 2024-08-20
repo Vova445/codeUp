ad8148cead2fdfdfd638132f02440f6f0406e3b6
Andri Kocherha <108304490+AndrijKocherha@users.noreply.github.com>
2024-08-20 18:01:15 +0300

    did headert and transleted header and main section

--- frontend/src/components/header/HeaderComp.vue
+++ frontend/src/components/header/HeaderComp.vue
@@ -5,18 +5,23 @@
             <img src="../../assets/img/logo.svg" alt="" />
          </router-link>
          <nav ref="menu" class="header__menu menu-header">
-            <ul class="menu-header__list">
-               <li ref="menuItem1" class="menu-header__item">Educational Courses</li>
-               <li ref="menuItem2" class="menu-header__item">
-                  <router-link :to="{ name: 'news' }">News</router-link>
-               </li>
-               <li ref="menuItem3" class="menu-header__item">
-                  <router-link :to="{ name: 'contacts' }">Contacts</router-link>
-               </li>
-               <li ref="menuItem4" class="menu-header__item">
-                  <button>Languages</button>
-               </li>
-            </ul>
+            <div class="menu-header__menu">
+               <ul class="menu-header__list">
+                  <li ref="menuItem1" class="menu-header__item">{{$t('header.educationalCourses')}}</li>
+                  <li ref="menuItem2" class="menu-header__item">
+                     <router-link :to="{ name: 'news' }">{{$t('header.news')}}</router-link>
+                  </li>
+                  <li ref="menuItem3" class="menu-header__item">
+                     <router-link :to="{ name: 'contacts' }">{{$t('header.contacts')}}</router-link>
+                  </li>
+                  <li ref="menuItem4" class="menu-header__item">
+                     <button>{{$t('header.languages')}}</button>
+                  </li>
+               </ul>
+               <router-link v-if="isMobile" :to="{ name: 'register' }" class="header__login-btn button">
+               {{$t('buttons.registerLogin')}}
+            </router-link>
+            </div>
          </nav>
          <router-link v-if="isUser" :to="{ name: 'user' }" class="header__user-btn">
             <img
@@ -27,25 +32,42 @@
             />
             <font-awesome-icon v-else :icon="['fas', 'user']" />
          </router-link>
-         <router-link v-else ref="registerBtn" :to="{ name: 'register' }" class="header__login-btn button">
-            Login/Register
-         </router-link>
+            <router-link v-if="!isMobile" :to="{ name: 'register' }" class="header__login-btn button">
+               {{$t('buttons.registerLogin')}}
+            </router-link>
+         <button @click="onIconMenu" class="icon-menu"><span></span></button>
       </div>
    </header>
 </template>
 
 <script setup>
-import { ref, onMounted, computed } from 'vue'
+import { ref, onMounted, computed, onUnmounted } from 'vue'
 import axios from 'axios'
-
-const isUser = computed(() => localStorage.getItem('authToken'))
+// variables
+const isMobile = ref(false)
 const avatar = ref('')
+const apiUrl = import.meta.env.VITE_API_URL.trim().replace(/\/+$/, '')
+// functions
+const checkWindowSize = () => {
+   isMobile.value = window.innerWidth < 501.98
+}
 function isAbsoluteURL(url) {
    return /^https?:\/\//i.test(url)
 }
-const apiUrl = import.meta.env.VITE_API_URL.trim().replace(/\/+$/, '')
+function onIconMenu() {
+   document.documentElement.classList.toggle('menu-open')
+}
+
+   //=computeds
+const isUser = computed(() => localStorage.getItem('authToken'))
+
+
+
 
 onMounted(async () => {
+   
+   checkWindowSize()
+   window.addEventListener('resize', checkWindowSize)
    const token = isUser.value
    if (token) {
       try {
@@ -80,6 +102,9 @@ onMounted(async () => {
       }
    })
 })
+onUnmounted(() => {
+   window.removeEventListener('resize', checkWindowSize)
+})
 </script>
 
 <style lang="scss" scoped>
@@ -99,9 +124,15 @@ onMounted(async () => {
       gap: 15px;
       align-items: center;
       grid-template-columns: auto 1fr auto;
+       @media (max-width: 767.98px) {
+      grid-template-columns: auto 1fr auto auto;
+
+       }
    }
 
    &__logo {
+      position: relative;
+      z-index: 50;
       width: 43px;
       height: 43px;
       transform: translateX(-20px);
@@ -113,6 +144,10 @@ onMounted(async () => {
 
       img {
          width: 100%;
+      }
+      @media (max-width: 500px){
+           width: 33px;
+            height: 33px;
       }
    }
 
@@ -131,6 +166,8 @@ onMounted(async () => {
    }
 
    &__login-btn {
+      position: relative;
+      z-index: 50;
       background-color: #234c5c;
       border-radius: 12px;
       line-height: 1.2;
@@ -144,6 +181,18 @@ onMounted(async () => {
 }
 
 .menu-header {
+   
+   &__menu {
+      @media (max-width: 767.98px) {
+         position: fixed;
+         padding: 100px;
+         left: -100%;
+         top: 0;
+         width: 100%;
+         height: 100%;
+      }
+   }
+
    &__list {
       display: flex;
       flex-wrap: wrap;
@@ -181,6 +230,70 @@ onMounted(async () => {
       }
    }
 }
+.icon-menu {
+   
+}
+.icon-menu {
+     display: none;
+   @media (max-width: 767.98px) {
+      display: block;
+      position: relative;
+      width: 25px;
+      height: 20px;
+      z-index: 50;
+      @media (any-hover: none) {
+         cursor: default;
+      }
+      span,
+      &::before,
+      &::after {
+         content: '';
+         transition: all 0.3s ease 0s;
+         right: 0;
+         position: absolute;
+         width: 100%;
+         height: 2px;
+         background-color: #ffffff;
+      }
+      &::before {
+         top: 0;
+      }
+      &::after {
+         width: 70%;
+         bottom: 0;
+      }
+      span {
+         top: 50%;
+         width: 90%;
+         transform: translate(0, -50%);
+      }
+   }
+   .menu-open & {
+      span {
+         width: 0;
+      }
+      &::before,
+      &::after {
+         width: 100%;
+      }
+      &::before {
+         //top: 50%;
+         //transform: rotate(-45deg) translate(0, -50%);
+         transform: rotate(-45deg) translate(0, -50%);
+         top: calc( 50%  - 1px )
+         
+      }
+      &::after {
+         //bottom: 50%;
+         //transform: rotate(45deg) translate(0, 50%);
+         transform: rotate(45deg) translate(0, 50%);
+         bottom: calc( 50% - 0px ) 
+
+      }
+   }
+}
+
+
 
 .loaded {
    .header {
---
+++ frontend/src/components/home/StepsSection.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="steps">
+      <div class="steps__container">
+
+      </div>
+   </div>
+</template>
+   
+
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps({
+   stepsList: {
+      type: Array,
+      required: true,
+   },
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>

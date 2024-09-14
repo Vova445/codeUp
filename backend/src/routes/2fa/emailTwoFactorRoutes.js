e2ec478687a38799dad8bda4fad33edabd2c58d0
Volodya Mikhnov <vvvmikhnov@gmail.com>
2024-09-14 12:29:53 +0300

    Update emailTwoFactorRoutes.js

--- backend/src/routes/2fa/emailTwoFactorRoutes.js
+++ backend/src/routes/2fa/emailTwoFactorRoutes.js
@@ -114,7 +114,7 @@ emailTwoFactorRoutes.post('/send-2fa-email', async (req, res) => {
 
     res.send(`<script>
       setTimeout(() => {
-        window.location.href = '${process.env.VERCEL_URL.trim().replace(/\/+$/, '')}/user';
+        window.location.href = '${process.env.VITE_API_URL.trim().replace(/\/+$/, '')}/user';
       }, 5000);
     </script>
     <p>Ваш обліковий запис успішно підтверджено! Ви будете перенаправлені назад через кілька секунд.</p>`);

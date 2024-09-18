39c9284578e7dc80c646551fd0a168550ab270ca
Andri Kocherha <108304490+AndrijKocherha@users.noreply.github.com>
2024-09-19 01:04:08 +0300

    d

--- frontend/src/components/home/MainSection.vue
+++ frontend/src/components/home/MainSection.vue
@@ -1,23 +1,36 @@
 <template>
    <section data-watch class="master">
       <div class="master__container">
-         <h1 class="master__title">{{ $t('titles.mainTitle') }}</h1>
-         <!--<div class="master__items">
-            <div v-for="courseItem in coursesList" :key="courseItem.id" class="master__item item-master">
-               <div class="item-master__content">
-                  <h3 class="item-master__title subtitile">{{ courseItem.title }}</h3>
-                  <div class="item-master__image"><img :src="getImagePath(courseItem.imgSrc)" alt="" /></div>
-                  <ul class="item-master__info">
-                     <li v-for="(infoItem, index) in courseItem.courseInfo" :key="index" class="item-master__item-info">
-                        {{ $t(`mainCourses.infos.${infoItem.title}`) }}
-                     </li>
-                  </ul>
+         <h1 class="master__title title">{{ $t('titles.mainTitle') }}</h1>
+         <div class="master__box">
+            <div class="master__icons-container">
+               <div
+                  class="circle-container"
+                  :class="course.classAdd"
+                  v-for="course in coursesList"
+                  :key="course.id"
+                  :style="[{ backgroundColor: course.background }, { transform: `scale(${course.scale})` }]"
+               >
+                  <svg viewBox="0 0 100 100" class="circle-svg">
+                     <defs>
+                        <path id="circlePath" d="M 50, 51 m -40, 0 a 40,40 0 1,1 80,0 a 40,40 0 1,1 -80,0" />
+                     </defs>
+                     <text>
+                        <textPath href="#circlePath" startOffset="25%" text-anchor="middle" dominant-baseline="middle">{{ course.title }}</textPath>
+                     </text>
+                  </svg>
+                  <font-awesome-icon :icon="course.image" class="icon-center" />
                </div>
-               <button class="item-master__button button">{{ $t('buttons.detaildedAboutCourse') }}</button>
-               <div class="item-master__price">{{ $t('infos.priceFrom') }} - {{ courseItem.price }} $</div>
             </div>
-         </div>-->
-         
+            <div class="master__content">
+               <div class="master__course-description">
+                  <span>CodeUp</span> - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates, vero magni? Ullam a ex laboriosam esse
+                  quasi recusandae eos natus fugit laudantium obcaecati harum, ipsa soluta tempora quaerat dignissimos nobis? Lorem, ipsum dolor sit
+                  amet consectetur adipisicing elit. Deserunt, nisi?
+               </div>
+               <button class="master__button button">detailed</button>
+            </div>
+         </div>
       </div>
    </section>
 </template>
@@ -38,150 +51,176 @@ const getImagePath = (imgPath) => {
 <style lang="scss" scoped>
 .master {
    &:not(:last-child) {
-      margin-bottom: clamp(4rem, -0.004rem + 11.742vw, 9.375rem);
+      margin-bottom: clamp(4.375rem, 1.561rem + 5.871vw, 6.25rem);
    }
-
    &__title {
-      text-align: center;
-      color: #e0e0e0;
-      text-transform: uppercase;
-      letter-spacing: 2px;
-      line-height: 1.3;
-      font-weight: 700;
-      font-size: clamp(1.35rem, 0.937rem + 1.174vw, 2rem);
-
       &:not(:last-child) {
-         margin-bottom: clamp(2.5rem, 0.624rem + 3.914vw, 3.75rem);
+         margin-bottom: 40px;
       }
    }
+   &__box {
+      position: relative;
+   }
+   &__icons-container {
+      display: flex;
+      gap: 30px;
 
-   &__items {
-      display: grid;
-      gap: clamp(2.5rem, 0.624rem + 3.914vw, 3.75rem);
-      grid-template-columns: repeat(2, 1fr);
+      &:not(:last-child) {
+         margin-bottom: 50px;
+      }
+      @media (max-width: 992px) {
+         overflow: auto;
+         overflow-x: auto;
+         -webkit-overflow-scrolling: touch;
+         padding-bottom: 15px;
+         &::-webkit-scrollbar {
+            height: 1.5px;
+         }
 
-      @media (max-width: 700px) {
-         grid-template-columns: 1fr;
-         gap: 50px;
+         &::-webkit-scrollbar-thumb {
+            background-color: #333333;
+            border-radius: 4px;
+         }
+
+         &::-webkit-scrollbar-track {
+            background-color: transparent;
+         }
+      }
+   }
+   &__content {
+      padding: 150px 0;
+      max-width: 600px;
+      margin: 0 auto;
+      color: #e6fefe;
+      text-align: center;
+      line-height: 1.6;
+      letter-spacing: 0.3px;
+
+      @media (max-width: 992px) {
+         padding: 0;
+      }
+      @media (max-width: 630px) {
+         text-align: left;
+      }
+      @media (max-width: 550px) {
+         font-size: 14px;
+         left: 1.5;
+      }
+   }
+   &__course-description {
+      &:not(:last-child) {
+         margin-bottom: 30px;
+      }
+      span {
+         font-size: 18px;
+         font-weight: 600;
+         color: #66b2a8;
+         @media (max-width: 550px) {
+            font-size: 16px;
+         }
+      }
+   }
+   &__button {
+      padding: 15px 70px;
+      background: linear-gradient(110deg, #3c776f, #6da086, #3c776f);
+      background-size: 200% 200%;
+
+      animation: gradientShift 5s ease infinite;
+      @keyframes gradientShift {
+         0% {
+            background-position: 0% 50%;
+         }
+         50% {
+            background-position: 100% 50%;
+         }
+         100% {
+            background-position: 0% 50%;
+         }
+      }
+      @media (any-hover: hover) {
+         &:hover {
+            background: linear-gradient(110deg, #3c776f, #3c776f, #3c776f);
+         }
       }
    }
 }
 
-.item-master {
+.circle-container {
+   color: #fff;
    position: relative;
-   background-color: #2b2b2b;
-   padding: 20px;
-   border-radius: 8px;
+   position: absolute;
+   top: 0;
+   left: 0;
+   width: 100px;
+   height: 100px;
    display: flex;
-   flex-direction: column;
+   justify-content: center;
    align-items: center;
-   text-align: center;
-   transition:
-      transform 0.4s ease,
-      box-shadow 0.3s ease;
-
-   @media (any-hover: hover) {
-      &:hover {
-         transform: translateY(-10px);
-         box-shadow: 7px 7px 10px #464646;
-      }
+   background-color: white;
+   border-radius: 50%;
+   @media (max-width: 992px) {
+      width: clamp(4.375rem, 2.958rem + 4.121vw, 6.25rem);
+      height: clamp(4.375rem, 2.958rem + 4.121vw, 6.25rem);
    }
+}
 
-   @media (max-width: 700px) {
-      width: 100%;
-      max-width: 100%;
-      margin: auto;
-   }
+.python {
+   left: 15%;
+   top: 0;
+}
+.vue {
+   left: 40%;
+   top: 10%;
+}
+.react {
+   top: 40%;
+   left: 5%;
+}
+.node {
+   top: 80%;
+   left: 20%;
+}
+.js {
+   top: 85%;
+   left: 50%;
+}
+.html-css {
+   top: 80%;
+   left: 80%;
+}
+.c-sharp {
+   top: 5%;
+   left: 70%;
+}
+.circle-svg {
+   position: absolute;
+   top: 0;
+   left: 0;
+   width: 100%;
+   height: 100%;
+   fill: none;
+}
+.c-plus {
+   left: 85%;
+   top: 40%;
+}
+text {
+   font-family: Arial, sans-serif;
+   font-size: 18px;
+   fill: #fff;
+}
 
-   &__content {
-      display: flex;
-      flex-direction: column;
-
-      &:not(:last-child) {
-         margin-bottom: clamp(1.125rem, 0.187rem + 1.957vw, 1.75rem);
-      }
-   }
-
-   &__title {
-      align-self: center;
-      letter-spacing: 3px;
-      color: #d3d3d3;
-      font-weight: 600;
-
-      &:not(:last-child) {
-         margin-bottom: 20px;
-      }
-   }
-
-   &__image {
+.circle-container {
+   @media (max-width: 992px) {
       position: relative;
-      padding-bottom: 81%;
-      margin-bottom: clamp(0.938rem, -0.001rem + 1.957vw, 1.563rem);
-
-      img {
-         position: absolute;
-         width: 100%;
-         height: 100%;
-         top: 0;
-         left: 0;
-         object-fit: contain;
-      }
-
-      @media (max-width: 700px) {
-         padding-bottom: 70%;
-      }
-
-      @media (max-width: 500px) {
-         padding-bottom: 60%;
-      }
+      top: 0;
+      left: 0;
+      transform: scale(1) !important;
+      flex: 0 0 clamp(4.375rem, 2.958rem + 4.121vw, 6.25rem);
    }
-
-   &__item-info {
-      text-align: left;
-      position: relative;
-      display: flex;
-      align-items: center;
-      line-height: 1.25;
-      padding-left: 20px;
-      color: #a0a0a0;
-      @media (max-width: 700px) {
-         font-size: 14px;
-      }
-      &:not(:last-child) {
-         margin-bottom: clamp(0.625rem, 0.156rem + 0.978vw, 0.938rem);
-      }
-
-      &::before {
-         content: '';
-         flex: 0 0 5px;
-         position: absolute;
-         background-color: #ffffff;
-         border-radius: 50%;
-         width: 5px;
-         height: 5px;
-         left: 0;
-      }
-
-      @media (max-width: 425px) {
-         font-size: 14px;
-      }
-   }
-
-   &__button {
-      width: 100%;
-      font-size: clamp(1rem, 0.812rem + 0.391vw, 1.125rem);
-      font-weight: 500;
-
-      &:not(:last-child) {
-         margin-bottom: clamp(0.625rem, -0.313rem + 1.957vw, 1rem);
-      }
-   }
-
-   &__price {
-      font-size: clamp(1.1rem, 0.468rem + 1.37vw, 1.563rem);
-      font-weight: 600;
-      color: #f9cf39;
-      margin-top: 1rem;
-   }
+}
+.icon-center {
+   position: absolute;
+   font-size: clamp(1.563rem, 1.09rem + 1.374vw, 2.188rem);
 }
 </style>

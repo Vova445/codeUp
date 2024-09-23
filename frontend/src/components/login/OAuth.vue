e24a246950310694a62250a09e869f5d8aad9665
Volodya Mikhnov <vvvmikhnov@gmail.com>
2024-09-23 18:40:52 +0300

    Update OAuth.vue

--- frontend/src/components/login/OAuth.vue
+++ frontend/src/components/login/OAuth.vue
@@ -1,20 +1,3 @@
-<template>
-    <div class="form-login__socials">
-        <button type="button" class="form-login__social form-login__social--google" @click="loginWithGoogle">
-      <font-awesome-icon :icon="['fab', 'google']" />
-    </button>
-       <button type="button" class="form-login__social form-login__social--facebook">
-          <font-awesome-icon :icon="['fab', 'facebook-f']" />
-       </button>
-       <button type="button" class="form-login__social form-login__social--github">
-          <font-awesome-icon :icon="['fab', 'github']" />
-       </button>
-       <button type="button" class="form-login__social form-login__social--linkedin-in">
-          <font-awesome-icon :icon="['fab', 'linkedin-in']" />
-       </button>
-    </div>
- </template>
- 
  <template>
     <div class="form-login__socials">
         <button type="button" class="form-login__social form-login__social--google" @click="loginWithGoogle">

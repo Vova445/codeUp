b0f7e4653b57e68a323c9923ae54bbe3908ed97b
Volodya Mikhnov <vvvmikhnov@gmail.com>
2024-09-23 15:17:16 +0300

    Update googleAuth.js

--- backend/src/routes/oauth2/googleAuth.js
+++ backend/src/routes/oauth2/googleAuth.js
@@ -3,6 +3,7 @@ import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import dotenv from 'dotenv';
 import { User } from '../../models/userModel.js';
+import jwt from 'jsonwebtoken';
 
 dotenv.config();
 const googleAuth = express.Router();
@@ -23,7 +24,7 @@ passport.use(new GoogleStrategy({
       done(err, null);
     }
   }));
-  
+
 passport.serializeUser((user, done) => done(null, user.id));
 passport.deserializeUser(async (id, done) => {
     try {
@@ -36,10 +37,10 @@ passport.deserializeUser(async (id, done) => {
 
 googleAuth.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-googleAuth.get('/auth/google/callback', passport.authenticate('google', {
-    failureRedirect: '/login'
-}), (req, res) => {
-    res.redirect('https://code-up-omega.vercel.app/');
+googleAuth.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), async (req, res) => {
+    const token = jwt.sign({ userId: req.user.id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+    res.cookie('authToken', token, { httpOnly: true });
+    res.redirect('https://code-up-omega.vercel.app/user');
 });
 
 export default googleAuth;

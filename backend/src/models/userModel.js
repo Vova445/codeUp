21d5c176ffc459a8c17fb01d53aef7837bc81073
Volodya Mikhnov <vvvmikhnov@gmail.com>
2024-09-23 15:05:10 +0300

    Update userModel.js

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -4,7 +4,7 @@ import bcrypt from 'bcrypt';
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: false },
   googleId: { type: String, unique: true, sparse: true },
   phoneNumber: { type: String },
   avatar: { type: Buffer },

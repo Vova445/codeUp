ec1ad5a4ffe225fd8207629cc49c4f35063185d9
Volodya Mikhnov <vvvmikhnov@gmail.com>
2024-09-23 18:37:46 +0300

    Update OAuth.vue

--- frontend/src/components/login/OAuth.vue
+++ frontend/src/components/login/OAuth.vue
@@ -15,9 +15,25 @@
     </div>
  </template>
  
- <script>
+ <template>
+    <div class="form-login__socials">
+        <button type="button" class="form-login__social form-login__social--google" @click="loginWithGoogle">
+            <font-awesome-icon :icon="['fab', 'google']" />
+        </button>
+        <button type="button" class="form-login__social form-login__social--facebook">
+            <font-awesome-icon :icon="['fab', 'facebook-f']" />
+        </button>
+        <button type="button" class="form-login__social form-login__social--github">
+            <font-awesome-icon :icon="['fab', 'github']" />
+        </button>
+        <button type="button" class="form-login__social form-login__social--linkedin-in">
+            <font-awesome-icon :icon="['fab', 'linkedin-in']" />
+        </button>
+    </div>
+</template>
+
+<script>
 import { onMounted } from 'vue';
-import Cookies from 'js-cookie';
 
 export default {
     name: 'OAuth',
@@ -33,14 +49,14 @@ export default {
             console.log('Token from URL:', token); 
 
             if (token) {
-                Cookies.set('authToken', token, { secure: true, sameSite: 'None' });
-                console.log('Token set in cookies:', Cookies.get('authToken')); 
+                localStorage.setItem('authToken', token);
+                console.log('Token set in localStorage:', localStorage.getItem('authToken')); 
                 setTimeout(() => {
                     window.location.href = 'https://code-up-omega.vercel.app/user';
-                }, 1000); 
+                }, 2000);
             } else {
-                const authToken = Cookies.get('authToken');
-                console.log('Auth Token from cookies:', authToken);
+                const authToken = localStorage.getItem('authToken');
+                console.log('Auth Token from localStorage:', authToken);
                 if (authToken) {
                     window.location.href = 'https://code-up-omega.vercel.app/user';
                 }
@@ -52,4 +68,5 @@ export default {
         };
     }
 };
+
 </script>

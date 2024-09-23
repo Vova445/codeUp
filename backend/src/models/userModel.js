651489f92097d2de2efc399ebc112041ad1e8af0
Volodya Mikhnov <vvvmikhnov@gmail.com>
2024-09-23 14:59:58 +0300

    .

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  googleId: { type: String, unique: true, sparse: true },
   phoneNumber: { type: String },
   avatar: { type: Buffer },
   avatarContentType: {type: String},

6b09512873a1d400cee25eaafe032c0e94802cbc
Andri Kocherha <108304490+AndrijKocherha@users.noreply.github.com>
2024-09-24 23:51:02 +0300

    d

--- frontend/src/components/login/OAuth.vue
+++ frontend/src/components/login/OAuth.vue
@@ -1,49 +1,40 @@
 <template>
-    <div class="form-login__socials">
-        <button type="button" class="form-login__social form-login__social--google" @click="loginWithGoogle">
-      <font-awesome-icon :icon="['fab', 'google']" />
-    </button>
-       <button type="button" class="form-login__social form-login__social--facebook">
-          <font-awesome-icon :icon="['fab', 'facebook-f']" />
-       </button>
-       <button type="button" class="form-login__social form-login__social--github">
-          <font-awesome-icon :icon="['fab', 'github']" />
-       </button>
-       <button type="button" class="form-login__social form-login__social--linkedin-in">
-          <font-awesome-icon :icon="['fab', 'linkedin-in']" />
-       </button>
-    </div>
- </template>
- 
- <script>
-import Cookies from 'js-cookie';
-import { onMounted } from 'vue';
-export default {
-    name: 'OAuth',
-    setup() {
-        const loginWithGoogle = () => {
-            const apiUrl = import.meta.env.VITE_API_URL.trim().replace(/\/+$/, '');
-            const googleAuthUrl = `${apiUrl}/api/auth/google`;
-            window.location.href = googleAuthUrl;
-        };
-        const checkAuthToken = () => {
-            const token = Cookies.get('authToken');
-            console.log('Current cookies:', Cookies.get());
-            if (token) {
-                console.log('Token is present:', token);
-                window.location.href = '/dashboard';
-            } else {
-                console.log('No token found');
-            }
-        };
-        onMounted(() => {
-            checkAuthToken();
-        });
+   <div class="form-login__socials">
+      <button type="button" class="form-login__social form-login__social--google" @click="loginWithGoogle">
+         <font-awesome-icon :icon="['fab', 'google']" />
+      </button>
+      <button type="button" class="form-login__social form-login__social--facebook">
+         <font-awesome-icon :icon="['fab', 'facebook-f']" />
+      </button>
+      <button type="button" class="form-login__social form-login__social--github">
+         <font-awesome-icon :icon="['fab', 'github']" />
+      </button>
+      <button type="button" class="form-login__social form-login__social--linkedin-in">
+         <font-awesome-icon :icon="['fab', 'linkedin-in']" />
+      </button>
+   </div>
+</template>
 
+<script setup>
+import Cookies from 'js-cookie'
+import { onMounted } from 'vue'
 
-        return {
-            loginWithGoogle,
-        };
-    },
-};
+const loginWithGoogle = () => {
+   const apiUrl = import.meta.env.VITE_API_URL.trim().replace(/\/+$/, '')
+   const googleAuthUrl = `${apiUrl}/api/auth/google`
+   window.location.href = googleAuthUrl
+}
+const checkAuthToken = () => {
+   const token = Cookies.get('authToken')
+   console.log('Current cookies:', Cookies.get())
+   if (token) {
+      console.log('Token is present:', token)
+      window.location.href = '/dashboard'
+   } else {
+      console.log('No token found')
+   }
+}
+onMounted(() => {
+   checkAuthToken()
+})
 </script>

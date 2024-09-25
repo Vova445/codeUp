797a05966635a9e9d6fecbf6911afd6ec86033dd
Volodya Mikhnov <vvvmikhnov@gmail.com>
2024-09-25 22:51:42 +0300

    Update OAuth.vue

--- frontend/src/components/login/OAuth.vue
+++ frontend/src/components/login/OAuth.vue
@@ -9,7 +9,7 @@
       <button type="button" class="form-login__social form-login__social--github" @click="loginWithGitHub">
          <font-awesome-icon :icon="['fab', 'github']" />
       </button>
-      <button type="button" class="form-login__social form-login__social--linkedin-in">
+      <button type="button" class="form-login__social form-login__social--linkedin-in" @click="loginWithLinkedIn">
          <font-awesome-icon :icon="['fab', 'linkedin-in']" />
       </button>
    </div>
@@ -38,5 +38,11 @@ const loginWithGitHub = () => {
    const githubAuthUrl = `${apiUrl}/api/auth/github`;
    window.location.href = githubAuthUrl;
 }
+const loginWithLinkedIn = () => {
+   const apiUrl = import.meta.env.VITE_API_URL.trim().replace(/\/+$/, '');
+   const linkedinAuthUrl = `${apiUrl}/api/auth/linkedin`;
+   window.location.href = linkedinAuthUrl;
+}
+
 
 </script>

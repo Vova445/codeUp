0845e652c9ce25d82ad8a1521194bcf96a93ad5e
Andri Kocherha <kotschergaandrij@gmail.com>
2024-11-11 00:22:04 +0200

    d

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
    <main-master-page>
-      <!--<main-section :courses-list="mainCourses" />
-      <reasons-section :reasons-list="reasonsList" />
+      <main-section :courses-list="mainCourses" />
+      <!--<reasons-section :reasons-list="reasonsList" />
       <statistics-section />
       <steps-section :steps-list="stepsList" />
       <purpose-section :purpose-list="purposeCourseList" />
@@ -14,23 +14,22 @@
             </router-link>
          </div>
       </questions-section>-->
-      <h1 class="title">Home page</h1>
    </main-master-page>
 </template>
 
 <script setup>
-//import { computed } from 'vue'
-//import { storeToRefs } from 'pinia'
-//import MainMasterPage from '../masterPages/MainMasterPage.vue'
-//import MainSection from '../components/home/MainSection.vue'
+import { computed } from 'vue'
+import { storeToRefs } from 'pinia'
+import MainMasterPage from '../masterPages/MainMasterPage.vue'
+import MainSection from '../components/home/MainSection.vue'
 //import ReasonsSection from '../components/home/ReasonsSection.vue'
 //import StepsSection from '../components/home/StepsSection.vue'
 //import QuestionsSection from '../components/home/QuestionsSection.vue'
 //import CarousalSection from '../components/home/CarousalSection.vue'
 //import PurposeSection from '../components/home/PurposeSection.vue'
-//import { useMainPageDataStore } from '../stores/mainPageData.js'
+import { useMainPageDataStore } from '../stores/mainPageData.js'
 //import StatisticsSection from '../components/home/StatisticsSection.vue'
-//const { mainCourses, reasonsList, stepsList, questionsList, purposeCourseList } = storeToRefs(useMainPageDataStore())
+const { mainCourses, reasonsList, stepsList, questionsList, purposeCourseList } = storeToRefs(useMainPageDataStore())
 //const getQuestionsList = computed(() => {
 //   return questionsList.value.slice(0, 3)
 //})

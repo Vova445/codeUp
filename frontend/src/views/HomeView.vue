006e7c59828c05da634600ad722b6862643cc887
Andri Kocherha <kotschergaandrij@gmail.com>
2024-11-10 23:56:57 +0200

    d

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -6,14 +6,14 @@
       <steps-section :steps-list="stepsList" />
       <purpose-section :purpose-list="purposeCourseList" />
       <!--<carousal-section :courses-list="mainCourses" />-->
-      <questions-section :questions-list="getQuestionsList">
+      <!--<questions-section :questions-list="getQuestionsList">
          <div class="faq__footer">
             <router-link :to="{ name: 'faq' }" class="faq__more">
                <div class="faq__text-more">more FAQ</div>
                <font-awesome-icon :icon="['fas', 'arrow-right-long']" />
             </router-link>
          </div>
-      </questions-section>
+      </questions-section>-->
    </main-master-page>
 </template>
 
@@ -24,7 +24,7 @@ import MainMasterPage from '../masterPages/MainMasterPage.vue'
 import MainSection from '../components/home/MainSection.vue'
 import ReasonsSection from '../components/home/ReasonsSection.vue'
 import StepsSection from '../components/home/StepsSection.vue'
-import QuestionsSection from '../components/home/QuestionsSection.vue'
+//import QuestionsSection from '../components/home/QuestionsSection.vue'
 //import CarousalSection from '../components/home/CarousalSection.vue'
 import PurposeSection from '../components/home/PurposeSection.vue'
 import { useMainPageDataStore } from '../stores/mainPageData.js'
